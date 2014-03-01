43a75f434bcb6d087c0dc15f1be8f7406a47bdff
cookjj <cookjj@colorado.edu>
2014-02-28 17:11:17 -0700

    meta progress

---
+++ multi-lookup.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+FILE *process_args(int, char**);
+
+
+int request_on_file(char *fname);
+int look(void);
+
+int try_queue_push(char *host);
+int try_write_out(char *line);
+
+int finished(void);
+int try_pop(char *buf);
+
+
+
+
+static inline int num_processors(void)
+{
+    return sysconf(_SC_NPROCESSORS_ONLN);
+}
+
+
+int
+nthreads(int min)
+{
+    int cores = num_processors();
+    return cores > min ? cores : min;
+}
+
